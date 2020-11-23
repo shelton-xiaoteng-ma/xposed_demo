a722f278c5b967ff992321b76ff133cd9e02a175
maxiaoteng <jizhuwo00@hotmail.com>
2020-11-23 11:09:42 +0800

    sekiro

--- app/src/main/java/com/example/xposed_demo/HookNineBotSekiro.java
+++ app/src/main/java/com/example/xposed_demo/HookNineBotSekiro.java
@@ -24,13 +24,17 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class HookNineBotSekiro implements IXposedHookLoadPackage {
 
+    /*
+    * 使用sekiro调用so文件， 共有两个方法，分别是加密和解密
+    * */
+
     private String hookPackageName = "cn.ninebot.ninebot";
     private String host = "192.168.2.141";
     private String groupName = "ninebot";
     private String actionName = "checkcode";
     private String clientId = UUID.randomUUID().toString();
     private Object encryptClassObj = null;
-    private String Tag = "nine";
+    private String Tag = "nine log: ";
 
     @Override
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
@@ -42,49 +46,60 @@ public class HookNineBotSekiro implements IXposedHookLoadPackage {
             XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    super.afterHookedMethod(param);
-                    Log.e(Tag, "hook到了九号加电");
-                    XposedBridge.log("hook到了九号加电");
+                super.afterHookedMethod(param);
+                Log.e(Tag, "hook到了九号加电");
+                XposedBridge.log("hook到了九号加电");
 
-                    if (encryptClassObj == null) {
-                        Class clazz = lpparam.classLoader.loadClass("com.bangcle.comapiprotect.CheckCodeUtil");
+                if (encryptClassObj == null) {
+                    Class clazz = lpparam.classLoader.loadClass("com.bangcle.comapiprotect.CheckCodeUtil");
 
-                        XposedHelpers.findAndHookMethod(clazz, "checkcode", String.class, int.class, new XC_MethodHook() {
-                            @Override
-                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                                super.afterHookedMethod(param);
+                    XposedHelpers.findAndHookMethod(clazz, "checkcode", String.class, int.class, new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        super.afterHookedMethod(param);
 
-                                Object[] args = param.args;
-                                Object result = param.getResult();
-                                Log.e(Tag, "args is  " + JSON.toJSONString(args));
-                                Log.e(Tag, "result is  " + JSON.toJSONString(result));
+                        Object[] args = param.args;
+                        Object result = param.getResult();
+                        Log.e(Tag, "args is  " + JSON.toJSONString(args));
+                        Log.e(Tag, "result is  " + JSON.toJSONString(result));
 
-                                if (param.thisObject != null) {
-                                    encryptClassObj = param.thisObject;
-                                    /*解决函数调用问题*/
-                                    //连接服务端并且注册处理的handler
-                                    try {
-                                        final SekiroClient sekiroClient = SekiroClient.start(host, clientId, groupName);
-                                        sekiroClient.registerHandler(actionName, new SekiroRequestHandler() {
-                                            @Override
-                                            public void handleRequest(SekiroRequest sekiroRequest, SekiroResponse sekiroResponse) {
-                                                String query = sekiroRequest.getString("query");
-                                                String res = JSON.toJSONString(XposedHelpers.callMethod(encryptClassObj, "checkcode", query, 1));
-                                                Log.e(Tag, "query: " + query + ", result: " + res);
-                                                sekiroResponse.success(res);
-                                            }
-                                        });
-                                    } catch (Exception e) {
-                                        Log.e(Tag, Objects.requireNonNull(e.getMessage()));
+                        if (param.thisObject != null) {
+                            encryptClassObj = param.thisObject;
+                            /*解决函数调用问题*/
+                            //连接服务端并且注册处理的handler
+                            try {
+                                final SekiroClient sekiroClient = SekiroClient.start(host, clientId, groupName);
+                                sekiroClient.registerHandler(actionName, new SekiroRequestHandler() {
+                                    @Override
+                                    public void handleRequest(SekiroRequest sekiroRequest, SekiroResponse sekiroResponse) {
+                                        String query = sekiroRequest.getString("query");
+                                        String res = JSON.toJSONString(XposedHelpers.callMethod(encryptClassObj, "checkcode", query, 1));
+                                        XposedBridge.log(Tag + "query: " + query + ", result: " + res);
+                                        sekiroResponse.success(res);
                                     }
-
-                                    XposedBridge.log("=========== sekiro服务启动成功 ===========");
-                                }
-                                Log.e(Tag, "方法1中hook成功");
-                                XposedBridge.log("方法1中hook成功");
+                                });
+                                // decheckcode
+                                final SekiroClient sekiroClient1 = SekiroClient.start(host, clientId, groupName);
+                                sekiroClient1.registerHandler("de" + actionName, new SekiroRequestHandler() {
+                                    @Override
+                                    public void handleRequest(SekiroRequest sekiroRequest, SekiroResponse sekiroResponse) {
+                                        String query = sekiroRequest.getString("query");
+                                        String res = JSON.toJSONString(XposedHelpers.callMethod(encryptClassObj, "decheckcode", query));
+                                        Object res_json = JSON.parse(res);
+                                        XposedBridge.log(Tag + "decheckcode query: " + query + ", result: " + res_json);
+                                        sekiroResponse.success(res_json);
+                                    }
+                                });
+                            } catch (Exception e) {
+                                Log.e(Tag, Objects.requireNonNull(e.getMessage()));
                             }
-                        });
-                    }
+
+                            XposedBridge.log("=========== sekiro服务启动成功 ===========");
+                        }
+                        XposedBridge.log("方法1中hook成功");
+                        }
+                    });
+                }
                 }
             });
         }
